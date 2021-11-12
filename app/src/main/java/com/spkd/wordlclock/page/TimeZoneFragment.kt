12f0918eea6c0a9b011c5ab4b7079196f156e6bd
Ramkumar Velmurugan <vrk1990@outlook.com>
2021-11-11 20:54:04 -0500

    TimeZone Search Implemented

--- app/src/main/java/com/spkd/wordlclock/page/TimeZoneFragment.kt
+++ app/src/main/java/com/spkd/wordlclock/page/TimeZoneFragment.kt
@@ -1,15 +1,13 @@
 package com.spkd.wordlclock.page
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import com.spkd.wordlclock.R
@@ -37,26 +35,33 @@ class TimeZoneFragment : Fragment(), TextWatcher {
         viewModel = ViewModelProvider(this).get(TimeZoneViewModel::class.java)
 
         viewModel.getCityList().observe(requireActivity(), {
-            timeZoneList.adapter = TimeZoneAdapter(requireContext(),it)
+            timeZoneList.adapter = TimeZoneAdapter(requireContext(), it)
         })
         timeZoneList.layoutManager = GridLayoutManager(context, 1)
         timeZoneList.setHasFixedSize(true)
-        timeZoneList.addItemDecoration(DividerItemDecoration(requireContext(),DividerItemDecoration.HORIZONTAL))
-
+        timeZoneList.addItemDecoration(
+            DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.HORIZONTAL
+            )
+        )
         timeZoneSearchInputText.addTextChangedListener(this)
-
     }
 
-    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        TODO("Not yet implemented")
+    override fun beforeTextChanged(textInput: CharSequence?, p1: Int, p2: Int, p3: Int) {
     }
 
-    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-        TODO("Not yet implemented")
+    override fun onTextChanged(textInput: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        viewModel.getCityList().observe(this, { it ->
+            val temp = it.filter {
+                it.contains(textInput.toString())
+            }
+            timeZoneList.adapter = TimeZoneAdapter(requireContext(), temp)
+        })
     }
 
     override fun afterTextChanged(textInput: Editable?) {
-
+        timeZoneList.adapter!!.notifyDataSetChanged()
     }
 
 }
